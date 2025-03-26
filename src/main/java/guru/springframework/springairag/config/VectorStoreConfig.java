9aad1c17cd46834cbef51f83b7904b56ec283ef1
nikishkaa <nikita.goutor@gmail.com>
2025-03-26 16:38:07 +0300

    Upd logger.

--- src/main/java/guru/springframework/springairag/config/VectorStoreConfig.java
+++ src/main/java/guru/springframework/springairag/config/VectorStoreConfig.java
@@ -1,6 +1,5 @@
 package guru.springframework.springairag.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
@@ -9,6 +8,8 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.List;
@@ -17,9 +18,10 @@ import java.util.List;
  * Created by jt, Spring Framework Guru.
  */
 
-@Slf4j
 @Configuration
 public class VectorStoreConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(VectorStoreConfig.class);
 
     @Bean
     public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel, VectorStoreProperties vectorStoreProperties) {
